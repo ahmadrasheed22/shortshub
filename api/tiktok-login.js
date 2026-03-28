1bde72ae890aeab4dcc4a6b60dfab706808bd13e
ahmadrasheed22 <ahmad22rasheed22@gmail.com>
2026-03-28 21:18:03 +0500

    chore: remove user.info.basic from TikTok OAuth scope

--- api/tiktok-login.js
+++ api/tiktok-login.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const params = new URLSearchParams({
     client_key: process.env.TIKTOK_CLIENT_KEY || '',
-    scope: 'user.info.basic,video.publish',
+    scope: 'video.publish',
     response_type: 'code',
     redirect_uri: process.env.TIKTOK_REDIRECT_URI || '',
     state: Math.random().toString(36).substring(2),
